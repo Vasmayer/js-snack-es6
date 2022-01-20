56c2cda595944fcb1abf880e4b6bf0eda5881fb3
Vasmayer <lealimarco90@gmail.com>
2022-01-20 14:30:06 +0100

    basic structure to start

---
+++ js/snack1.js
@@ -0,0 +1,9 @@
+/* 
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+
+BONUS
+Stampare in pagina oltre che in console!
+ */
