76a5fbb4fea3b8907fc305197d95aa0bf2c91953
Vasmayer <lealimarco90@gmail.com>
2022-01-20 15:09:09 +0100

    snack2 create iteration on array

--- js/snack2.js
+++ js/snack2.js
@@ -1,6 +1,7 @@
 /* 
 Snack2
-Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:0nome, punti fatti, falli subiti.
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:
+nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
@@ -15,19 +16,30 @@ const rdnNumber = (min,max) => Math.floor(Math.random() * (max - min)) + min;
 
 /* creo un'array di oggetti 'club' contenente le squadre di calcio */
 const club = [
-    {name:'Juventus',points:0,foulssuffered:3},
-    {name:'Milan',points:0,foulssuffered:0},
-    {name:'Inter',points:0,foulssuffered:0},
-    {name:'Roma',points:0,foulssuffered:0},
-    {name:'Lazio',points:0,foulssuffered:0},
-    {name:'Brescia',points:0,foulssuffered:0},
-    {name:'Fiorentina',points:0,foulssuffered:0},
-    {name:'Napoli',points:0,foulssuffered:0},
-    {name:'Atalanta',points:0,foulssuffered:0},
-    {name:'Samptoria',points:0,foulssuffered:0},
-    {name:'Torino',points:0,foulssuffered:0},
-    {name:'Bologna',points:0,foulssuffered:0}
+    {name:'Juventus',points:0,foulsSuffered:3},
+    {name:'Milan',points:0,foulsSuffered:0},
+    {name:'Inter',points:0,foulsSuffered:0},
+    {name:'Roma',points:0,foulsSuffered:0},
+    {name:'Lazio',points:0,foulsSuffered:0},
+    {name:'Brescia',points:0,foulsSuffered:0},
+    {name:'Fiorentina',points:0,foulsSuffered:0},
+    {name:'Napoli',points:0,foulsSuffered:0},
+    {name:'Atalanta',points:0,foulsSuffered:0},
+    {name:'Samptoria',points:0,foulsSuffered:0},
+    {name:'Torino',points:0,foulsSuffered:0},
+    {name:'Bologna',points:0,foulsSuffered:0}
 ];
 
 /* stampo in console l'array */
 console.table(club);
+
+const min = 0;
+const max = 100;
+
+/* creo un ciclo per leggere l'array di oggetti */
+for(let i = 0;i < club.length;i++)
+{
+    /* setto i valori casualmante */
+    club[i].points = rdnNumber(min,max);
+    club[i].foulsSuffered = rdnNumber(min,max);
+}
