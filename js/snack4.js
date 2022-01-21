6ab661806a6ea561b3845f56d66e616852bbb78a
Vasmayer <lealimarco90@gmail.com>
2022-01-21 16:15:38 +0100

    Snack4 added array of objects

--- js/snack4.js
+++ js/snack4.js
@@ -1,9 +1,24 @@
 /* 
 SNACK 4
-Abbiamo un elenco degli studenti di una facoltà, identificati da id, nome e somma totale dei loro voti di esame...
+Abbiamo un elenco degli studenti di una facoltà, identificati da
+ id, nome e somma totale dei loro voti di esame...
 1. dobbiamo creare una lista contenente delle targhe (stringhe) con il loro nome in maiuscolo
 ES (marco de iulio => Marco de iulio);
 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 BONUS  nei nomi sulle targhe l'iniziale di ogni parola deve essere maiuscola
 ES.: (marco de iulio  => Marco De Iulio) */
+
+const students = [
+    {id:10,nome:'Marco',sumVotes:100},
+    {id:20,nome:'Michele',sumVotes:110},
+    {id:30,nome:'Giuseppe',sumVotes:120},
+    {id:40,nome:'Anacleto',sumVotes:130},
+    {id:50,nome:'Alfredo',sumVotes:140},
+    {id:60,nome:'Pietro',sumVotes:150},
+    {id:70,nome:'Ornella',sumVotes:160},
+    {id:80,nome:'Francesca',sumVotes:170},
+    {id:90,nome:'Antonietta',sumVotes:180}
+];
+   
+
